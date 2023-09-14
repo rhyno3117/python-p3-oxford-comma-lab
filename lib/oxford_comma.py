ea89261486e1b386b67850e0bc97037f3b3e2c97
Ryan Meyers <rhynogolfer42@gmail.com>
2023-09-14 13:39:45 -0600

    Adds myfile.md

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,11 @@
+# Write a function oxford_comma() in the lib/oxford_comma.py file that takes an array 
+# of string elements as an argument and converts it into a string using the Oxford commaLinks to an external site..
+
 def oxford_comma(items):
-    return None
+    if len(items) > 1:
+        items[-1] = "and " + items[-1]
+
+    if len(items) > 2:
+        return ', '.join(items)
+    else:
+        return ' '.join(items)
